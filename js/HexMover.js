eb51805093bbee9da412fef2a897ec21dbfca938
Ciprian Tomoiaga <cipri.tom@cern.ch>
2015-03-20 00:00:53 +0100

    sorry, IDE made indentation consistent :D

--- js/HexMover.js
+++ js/HexMover.js
@@ -1,23 +1,23 @@
 // Author: Neha Agrawal 2015
 //   Notes: Opacity or brightness cannot be changed without changing the instrinsic value
 //   		  of the Hex Color. For example, to reduce the brightness of the given colours
-//   		  using a function. A change in brightness would result in a completely 
+//   		  using a function. A change in brightness would result in a completely
 //   		  different colour
-//   		  * ('#000000', 50) --> #808080
+// 		  * ('#000000', 50) --> #808080
 // 		  * ('#EEEEEE', 25) --> #F2F2F2
 // 		  * ('EEE     , 25) --> #F2F2F2
 //			Dont think my calculations for setting initial colours are completely correct.
-// 			I am open to feedback on this issue			
+// 			I am open to feedback on this issue
 
 //---------------------------------------------------------------------------------------
 
-var red 		= 0x00;
-var green 		= 0x00;
-var blue 		= 0x00;
-var blueStep 	= 1;
-var greenStep 	= 1;
-var redStep 	= 1;
-var opacity 	= 1.0;
+var red 		     = 0x00;
+var green 		     = 0x00;
+var blue 		     = 0x00;
+var blueStep 	     = 1;
+var greenStep 	     = 1;
+var redStep 	     = 1;
+var opacity 	     = 1.0;
 var upperColourLimit = 0x100;
 var lowerColourLimit = -1;
 var displayRed;
@@ -25,7 +25,7 @@ var displayGreen;
 var displayBlue;
 var image;
 
-$(document).ready(function(){
+$(document).ready(function() {
 
 	// Dom label elements
 	displayRed = document.getElementById('displayRed');
@@ -35,34 +35,32 @@ $(document).ready(function(){
 
 	// Time in milliseconds since midnight
 	var now = new Date(),
-    then = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        0,0,0),
-        diff = now.getTime() - then.getTime(); // difference in milliseconds
+	then = new Date(
+		now.getFullYear(),
+		now.getMonth(),
+		now.getDate(),
+		0,0,0),
+		diff = now.getTime() - then.getTime(); // difference in milliseconds
 
-    var colour = diff; // The colour we should currently be at
-    // Now calculate RGB and steps
-    setInitialColours(colour);
+	var colour = diff; // The colour we should currently be at
+	// Now calculate RGB and steps
+	setInitialColours(colour);
 
 	timeout()
-	
+
 });
 
-function setInitialColours (colourNumber) 
+function setInitialColours (colourNumber)
 {
 	// Set blue initial colour
 	var copy = colourNumber;
 	copy = Math.floor(copy%0x9F6);
 	copy = Math.floor(copy/0x5);
-	if(copy > 0xFF)
-	{
+	if(copy > 0xFF) {
 		blueStep = -1;
 		blue = 0xFF - (copy - 0x100);
 	}
-	else
-	{
+	else {
 		blue = copy;
 	}
 
@@ -70,13 +68,11 @@ function setInitialColours (colourNumber)
 	copy = colourNumber;
 	copy = Math.floor(copy%0x9F600);
 	copy = Math.floor(copy/0x4FB);
-	if(copy > 0xFF)
-	{
+	if(copy > 0xFF) {
 		greenStep = -1;
 		green = 0xFF - (copy - 0x100);
 	}
-	else
-	{
+	else {
 		green = copy;
 	}
 
@@ -85,56 +81,50 @@ function setInitialColours (colourNumber)
 	debugger;
 	copy = Math.floor(copy%0x4FB0000);
 	copy = Math.floor(copy/0x9F600);
-	if(copy > 0xFF)
-	{
+	if(copy > 0xFF) {
 		redStep = -1;
 		red = 0xFF - (copy - 0x100);
 	}
-	else
-	{
+	else {
 		red = copy;
 	}
 }
 
 
-function timeout() {
-	
+function timeout()
+{
 		setTimeout(function () {
-	        
-	        setColour();
-	        changeBlue();
-	        if(blue == upperColourLimit || blue == lowerColourLimit)
-	        {
-	        	blueStep *= -1;
-	        	changeBlue();
+			setColour();
+			changeBlue();
+			if(blue == upperColourLimit || blue == lowerColourLimit) {
+				blueStep *= -1;
+				changeBlue();
 
-	        	// GREEN
-	        	changeGreen();
-	        	if(green == upperColourLimit || green == lowerColourLimit)
-	        	{
-	        		greenStep *= -1; 
-	        		changeGreen();
+				// GREEN
+				changeGreen();
+				if(green == upperColourLimit || green == lowerColourLimit) {
+					greenStep *= -1;
+					changeGreen();
 
-	        		// RED
-	        		changeRed();
-	        		if(red == upperColourLimit || red == lowerColourLimit)
-	        		{
-	        			redStep *= -1;
-	        			changeRed();
-	        		}
-	        	}
-	        }
+					// RED
+					changeRed();
+					if(red == upperColourLimit || red == lowerColourLimit) {
+						redStep *= -1;
+						changeRed();
+					}
+				}
+			}
 
-	        timeout();
-    	}, 5);	
-	  
+			timeout();
+		}, 5);
 }
 
 //----------------------------------------------------------------------------
 
-function setColour () {
+function setColour ()
+{
 	var colourRGB = parseInt(red)+","+parseInt(green)+","+parseInt(blue);
-	$('.screen').css("background-color", "rgb("+colourRGB+")"); 
+	$('.screen').css("background-color", "rgb("+colourRGB+")");
 	displayRed.textContent = red;
 	displayGreen.textContent = green;
 	displayBlue.textContent = blue;
@@ -162,19 +152,19 @@ function changeBlue()
 
 //-----------------------------------------------------------------------------
 
-function changeOpacity () 
+function changeOpacity ()
 {
 	opacity = opacity + opacityStep;
 }
 
 //-----------------------------------------------------------------------------
 
-function BirthdayKeyPress(e) {
-      var evtobj = window.event? event : e
-      if (evtobj.keyCode == 73 && evtobj.ctrlKey)
-      {
-      	image.style.display = "";
-      }
+function BirthdayKeyPress(e)
+{
+	var evtobj = window.event? event : e
+	if (evtobj.keyCode == 73 && evtobj.ctrlKey) {
+		image.style.display = "";
+	}
 }
 
 function HideBirthday (e) {
@@ -182,4 +172,4 @@ function HideBirthday (e) {
 }
 
 document.onkeydown = BirthdayKeyPress;
-document.onkeyup = HideBirthday;
+document.onkeyup   = HideBirthday;
